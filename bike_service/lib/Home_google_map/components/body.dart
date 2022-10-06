792903f54ebc600af8648c487b9ba3ee942a13c8
Haku0910 <kirakhang269@gmail.com>
2022-10-06 07:30:26 +0700

    google map, add moto when no moto, list store

---
+++ bike_service/lib/Home_google_map/components/body.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/Home_google_map/components/google_map.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height,
+      width: double.infinity,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Home_appbar(),
+        ],
+      ),
+    );
+  }
+}
---
+++ bike_service/lib/Home_list_store/List_home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/Home_list_store/components/body.dart';
+
+class ListHome extends StatelessWidget {
+  const ListHome({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Body());
+  }
+}
